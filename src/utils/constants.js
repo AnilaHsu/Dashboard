25f2a9f8a12b5dcc30fb8fccf2cc714ae63e6ec9
simple1017 <simple01281017@gmail.com>
2021-06-16 01:05:38 +0800

    Update build

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 let baseURL
 if (process.env.NODE_ENV === 'production')
-  baseURL = "http://localhost:7321"
+  baseURL = "http://59.120.19.161:7321"
 else
   baseURL = "http://59.120.19.161:7321"
 
